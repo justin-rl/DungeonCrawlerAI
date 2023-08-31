36224d912a53d1a088a1cf42e56834c9589ac1c7
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2023-08-31 10:48:29 -0400

    init

---
+++ jaskier.py
@@ -0,0 +1,61 @@
+from pygame.rect import Rect
+
+
+class Jaskier:
+    def __init__(self, maze) -> None:
+        self.maze = maze
+
+    def maze_info(self):
+        return {
+            "walls": list(
+                filter(
+                    lambda x: x is not None,
+                    map(
+                        self.rect_to_position,
+                        map(self.rect_to_tile, self.maze.wallList),
+                    ),
+                )
+            ),
+            "coins": list(
+                map(self.rect_to_position, map(self.rect_to_tile, self.maze.coinList))
+            ),
+            "tresures": list(
+                map(
+                    self.rect_to_position,
+                    map(self.rect_to_tile, self.maze.treasureList),
+                )
+            ),
+            "obstacle": list(
+                map(
+                    self.rect_to_position,
+                    map(self.rect_to_tile, self.maze.obstacleList),
+                )
+            ),
+            "monsters": list(
+                map(
+                    self.rect_to_position,
+                    map(self.rect_to_tile, self.maze.monsterList),
+                )
+            ),
+            "doors": list(
+                map(self.rect_to_position, map(self.rect_to_tile, self.maze.doorList))
+            ),
+        }
+
+    def position_to_tile(self, position):
+        x, y = position
+        return x / self.maze.tile_size_x, y / self.maze.tile_size_y
+
+    def rect_to_tile(self, rect: Rect):
+        return Rect(
+            round(rect.left / self.maze.tile_size_x),
+            round(rect.top / self.maze.tile_size_y),
+            round(rect.width / self.maze.tile_size_x),
+            round(rect.height / self.maze.tile_size_y),
+        )
+
+    def rect_to_position(self, rect: Rect):
+        if rect.width == 1 and rect.height:
+            return rect.left, rect.top
+        else:
+            None
---
+++ main.py
@@ -0,0 +1,17 @@
+# Simple interactive dungeon crawler
+# This code was written for the AI courses in computer engineering at Université de Sherbrooke
+# Author : Audrey Corbeil Therrien
+
+from Games2D import *
+from witcher import Geralt
+
+if __name__ == '__main__':
+    # Niveau 0 - sans obstacle - 'assets/Mazes/mazeMedium_0'
+    # Niveau 1 - avec obstacles - 'assets/Mazes/mazeMedium_1'
+    # Niveau 2 - avec obstacles, portes et un ennemi - 'assets/Mazes/mazeMedium_2'
+    # Niveau 2 - avec obstacles, portes et plusieurs ennemis - 'assets/Mazes/mazeMedium_2'
+    level = 0
+    app = App(f'assets/Mazes/mazeMedium_{level}')
+    geralt = Geralt(app.player, app.maze) 
+    app.on_execute(ai_player=geralt)
+    

529658fe836899b04e067a42ac92896276fef446
BenjaminMoff <mofb1301@usherbrooke.ca>
2023-09-15 10:48:13 -0400

    valid

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ from geralt.vesemir import Vesemir
 from geralt.geralt import Geralt
 
 if __name__ == "__main__":
-    game_app = App("assets/Mazes/mazeMedium_2")
+    game_app = App("assets/Mazes/mazeLarge_1")
     geralt = Geralt(game_app)
     game_app.on_execute(ai_action_callback=geralt.action)

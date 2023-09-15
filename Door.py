1838b84bb840b1965b905e449c913417fb3a927e
BenjaminMoff <mofb1301@usherbrooke.ca>
2023-09-15 11:08:17 -0400

    porte valid

--- Door.py
+++ Door.py
@@ -2,27 +2,27 @@ import random
 
 
 class Door:
-    STATES = [['gold', 'red', 'red', 'blue', 'yellow', '', ''],
-              ['gold', 'blue', 'blue', 'yellow', '', '', ''],
-              ['gold', 'red', 'white', 'blue', 'yellow', 'white', 'black'],
-              ['silver', 'red', 'red', 'blue', 'yellow', '', ''],
-              ['silver', 'yellow', 'red', 'blue', 'yellow', 'black', ''],
-              ['silver', 'red', 'red', 'blue', '', '', ''],
-              ['bronze', 'red', 'red', 'blue', 'yellow', '', ''],
-              ['bronze', 'red', 'red', 'blue', 'yellow', 'black', ''],
-              ['bronze', 'red', 'red', 'blue', 'blue', 'black', 'white'],
-              ['bronze', 'yellow', 'white', 'blue', 'yellow', 'black', 'white']]
+    STATES = [['gold', 'yellow', 'blue', 'white', 'white', '', ''],
+              ['gold', 'blue', 'blue', 'blue', '', '', ''],
+              ['gold', 'blue', 'white', 'red', 'red', 'white', 'black'],
+              ['silver', 'black', 'red', 'red', 'red', '', ''],
+              ['gold', 'red', 'blue', 'red', 'blue', 'black', ''],
+              ['silver', 'blue', 'red', 'yellow', '', '', ''],
+              ['bronze', 'blue', 'blue', 'blue', 'blue', '', ''],
+              ['bronze', 'white', 'black', 'black', 'blue', 'yellow', ''],
+              ['bronze', 'blue', 'blue', 'red', 'yellow', 'black', 'white'],
+              ['bronze', 'black', 'white', 'red', 'red', 'black', '']]
 
     KEYS = ['first',
             'second',
             'fourth',
+            'fourth',
+            'fourth',
+            'first',
             'second',
             'first',
-            'first',
-            'first',
-            'first',
-            'third',
-            'sixth']
+            'fourth',
+            'first']
 
     def __init__(self, rect):
         self.rect = rect
